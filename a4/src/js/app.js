e6e65842c78a90eee82cd813c9f505f5649ae00e
fmoayeri2 <fmoayeri2@myseneca.ca>
2023-07-25 15:20:46 -0400

    a4: assignment complete

--- a4/src/js/app.js
+++ a4/src/js/app.js
@@ -10,63 +10,55 @@
  *
  *      Name:       Farbod Moayeri
  *      Student ID: 134395227
- *      Date:       <SUBMISSION_DATE>
+ *      Date:       2023/07/25
  */
 
 // All of our data is available on the global `window` object.
 // Create local variables to work with it in this file.
 const { products, categories } = window;
 
-window.addEventListener("load", function() {
+window.addEventListener("load", function () {
+  let menu = document.getElementById("menu");
 
-    let menu = document.getElementById("menu");
+  for (let i = 0; i < categories.length; i++) {
+    let createdElement = document.createElement("button");
+    createdElement.id = categories[i].id;
+    createdElement.textContent = categories[i].name;
+    menu.appendChild(createdElement);
+  }
 
-    for (let i = 0; i < categories.length; i++) {
-        let createdElement = document.createElement("button");
-        createdElement.id = categories[i].id;
-        createdElement.textContent = categories[i].name;
-        menu.appendChild(createdElement);
-    } 
+  var menuButtons = document.querySelector("#menu").querySelectorAll("button");
 
-    var menuButtons = document.querySelector("#menu").querySelectorAll("button");
+  productList(menuButtons[0]);
 
-    productList( menuButtons[0]);
-
-    for(let i = 0; i < menuButtons.length ; i++)
-    {
-        menuButtons[i].addEventListener("click", function() {
-            productList(menuButtons[i]);
-        });
-    }
-
+  for (let i = 0; i < menuButtons.length; i++) {
+    menuButtons[i].addEventListener("click", function () {
+      productList(menuButtons[i]);
+    });
+  }
 });
 
+function productList(category) {
+  document.getElementById("category-products").innerHTML = "";
+  document.getElementById("selected-category").innerHTML = category.textContent;
 
-function productList(category)
-{
+  var filter = products.filter(
+    (cat) => cat.categories[0].includes(category.id) && cat.discontinued !== true
+  );
 
-    document.getElementById("category-products").innerHTML = "";
-    document.getElementById("selected-category").innerHTML = category.textContent;
+  filter.forEach((element) => {
+    var Row = document.getElementById("category-products").insertRow();
+    Row.className = "row";
+    var Cell = Row.insertCell();
+    var Contents = document.createTextNode(element.title);
+    Cell.appendChild(Contents);
 
-    var filter = products.filter(cat => cat.categories[0].includes(category.id) && cat.discontinued !== true);
-    
-    filter.forEach(element => {
-        var Row = document.getElementById("category-products").insertRow();
-        Row.className = "row";
-        var Cell = Row.insertCell();
-        var Contents = document.createTextNode(element.title);
-        Cell.appendChild(Contents);
+    Cell = Row.insertCell();
+    Contents = document.createTextNode(element.description);
+    Cell.appendChild(Contents);
 
-        Cell = Row.insertCell();
-        Contents = document.createTextNode(element.description);
-        Cell.appendChild(Contents);
-
-        Cell = Row.insertCell();
-        Contents = document.createTextNode(`$${element.price/100}`);
-        Cell.appendChild(Contents);
-    });
-    
+    Cell = Row.insertCell();
+    Contents = document.createTextNode(`$${element.price / 100}`);
+    Cell.appendChild(Contents);
+  });
 }
-
-
-
--- a4/src/js/products.js
+++ a4/src/js/products.js
@@ -27,7 +27,8 @@ window.products = [
   {
     id: "D1",
     title: "T-Rex Plushie",
-    description: "A green T-Rex Plushie. Perfect way to introduce dinosaurs to children.",
+    description:
+      "A green T-Rex Plushie. It's tail and feet have internal support so the T-rex can stand on its own without support. It is a proud and mighty Dino.",
     price: 1299,
     discontinued: false,
     categories: ["Dino"]
@@ -35,7 +36,8 @@ window.products = [
   {
     id: "D2",
     title: "Triceratops Plushie",
-    description: "A green triceratops Plushie. Perfect way to introduce dinosaurs to children.",
+    description:
+      "A green triceratops Plushie. Its horns squeak when you squeeze them but they are as soft as the rest of this plushie.",
     price: 1399,
     discontinued: false,
     categories: ["Dino"]
@@ -43,7 +45,8 @@ window.products = [
   {
     id: "D3",
     title: "Pterodactyl Plushie",
-    description: "A purple Pterodactyl Plushie. Perfect way to introduce dinosaurs to children.",
+    description:
+      "A purple Pterodactyl Plushie. It is made so it glides when thrown. Guaranteed to be a delight to children.",
     price: 1099,
     discontinued: false,
     categories: ["Dino"]
@@ -51,7 +54,8 @@ window.products = [
   {
     id: "D4",
     title: "Velociraptor Plushie",
-    description: "A green Velociraptor Plushie. Perfect way to introduce dinosaurs to children.",
+    description:
+      "A green Velociraptor Plushie. The velociraptor makes a dinosaur noise when squeezed, and its pitch changes depending on where it is squeezed.",
     price: 999,
     discontinued: false,
     categories: ["Dino"]
@@ -60,7 +64,7 @@ window.products = [
     id: "D5",
     title: "Stegosaurus Plushie",
     description:
-      "A brown and green Stegosaurus Plushie. Perfect way to introduce dinosaurs to children.",
+      "A brown and green Stegosaurus Plushie. The spines on its back is soft and feels incredible to touch.",
     price: 1599,
     discontinued: false,
     categories: ["Dino"]
@@ -69,56 +73,90 @@ window.products = [
   {
     id: "M1",
     title: "Monkey Plushie",
-    description: "A brown monkey Plushie. Perfect way to introduce mammals to children.",
-    price: 1399,
+    description:
+      "A brown monkey Plushie. It makes a monkey noise when a button that is hidden inside the chest of the plushie is pressed. It takes deliberate force to push the button so the noise will not occur is the plushie is held tightly.",
+    price: 1799,
     discontinued: false,
     categories: ["Mamma"]
   },
   {
     id: "M2",
     title: "Elephant Plushie",
-    description: "An elephant plushie. This plushies trunk is both extendable and moldable, meaning more fun for everyone.",
-    price: 1399,
+    description:
+      "An elephant plushie. This plushies trunk is both extendable and moldable, meaning more fun for everyone.",
+    price: 1599,
     discontinued: false,
     categories: ["Mamma"]
   },
   {
     id: "M3",
     title: "Wolf Plushie",
-    description: "A wolf plushie. Wolves live in packs so if you buy 4, you get the next 4 for free.",
-    price: 1399,
+    description:
+      "A wolf plushie. Wolves live in packs so if you buy 4, you get the next 4 for free.",
+    price: 999,
     discontinued: false,
     categories: ["Mamma"]
   },
   {
     id: "M4",
     title: "Lion Plushie",
-    description: "A lion plushie. A soft mane guarantees that children and adults alike will fall in love with this fearsome creature.",
-    price: 1399,
+    description:
+      "A lion plushie. A soft mane guarantees that children and adults alike will fall in love with this fearsome creature.",
+    price: 1299,
     discontinued: false,
     categories: ["Mamma"]
   },
   {
     id: "M5",
     title: "Giraffe Plushie",
-    description: "A giraffe Plushie. The british fabricated the existence of giraffes to brag about hunting a 20 foot tall creature.",
-    price: 1399,
+    description:
+      "A giraffe Plushie. The british fabricated the existence of giraffes to brag about hunting a 20 foot tall creature.",
+    price: 1099,
     discontinued: true,
     categories: ["Mamma"]
   },
   {
     id: "M6",
     title: "Hamster Plushie",
-    description: "A hamster plushie. Child whining for a pet hamster? Not willing to clean up after it? This is the next best thing.",
-    price: 1399,
+    description:
+      "A hamster plushie. Child whining for a pet hamster? Not willing to clean up after it? This is the next best thing.",
+    price: 899,
     discontinued: true,
+    categories: ["Mamma"]
+  },
+  {
+    id: "M7",
+    title: "Gorilla Plushie",
+    description:
+      "A gorilla plushie. This gorilla spins around and around if you tap it on its head. Do not attempt to sleep beside it.",
+    price: 1599,
+    discontinued: true,
+    categories: ["Mamma"]
+  },
+  {
+    id: "M8",
+    title: "Pig Plushie",
+    description:
+      "A pig plushie. The pig oinks if you tap its nose three times. Perfect for teaching children about farm animals.",
+    price: 1499,
+    discontinued: false,
+    categories: ["Mamma"]
+  },
+  {
+    id: "M8",
+    title: "Bunny Plushie",
+    description:
+      "A bunny plushie. This plushie jumps up a foot if both ears are held up for three seconds.",
+    price: 1199,
+    discontinued: false,
     categories: ["Mamma"]
   },
   // AQUATIC --------------------------------------------------------------------------------------
   {
     id: "A1",
     title: "Dolphin Plushie",
-    description: "A blue dolphin Plushie. Perfect way to introduce aquatic life to children.",
+    description:
+      "A blue dolphin Plushie. Has a motor in its tail that stimulates the swimming of a dolphin. Do not submerge in water.",
     price: 1399,
     discontinued: false,
     categories: ["Aqua"]
@@ -126,33 +164,46 @@ window.products = [
   {
     id: "A2",
     title: "Shark Plushie",
-    description: "A white shark Plushie. Perfect way to introduce aquatic life to children.",
-    price: 1399,
+    description:
+      "A white shark Plushie. Sings baby shark when shake'd three times in quick succession.",
+    price: 1299,
     discontinued: false,
     categories: ["Aqua"]
   },
   {
     id: "A3",
     title: "Jellyfish Plushie",
-    description: "A blue jellyfish Plushie. Perfect way to introduce aquatic life to children.",
-    price: 1399,
+    description:
+      "A blue jellyfish Plushie. This jellyfish plushie can float for some time due to there being trapped helium inside of it. A delight for the eyes.",
+    price: 1199,
     discontinued: false,
     categories: ["Aqua"]
   },
   {
     id: "A4",
     title: "Whale Plushie",
-    description: "A blue whale Plushie. Perfect way to introduce aquatic life to children.",
-    price: 1399,
+    description:
+      "A blue whale Plushie. Make an inaudible whale noise when held. The pitch increases the longer it is held so you eventually can hear its gentle cries.",
+    price: 1499,
     discontinued: false,
     categories: ["Aqua"]
   },
   {
     id: "A5",
     title: "Goldfish Plushie",
-    description: "An orange goldfish Plushie. Perfect way to introduce aquatic life to children.",
-    price: 1399,
+    description:
+      "An orange goldfish Plushie. Tired of buying goldfish for your children only for the fish to be flushed down the toilet a couple days later? Fear not, the goldfish plushie is here.",
+    price: 999,
     discontinued: true,
+    categories: ["Aqua"]
+  },
+  {
+    id: "A6",
+    title: "Electric Eel Plushie",
+    description:
+      "An electric eel Plushie. It vibrates when touched and the vibrations get stronger the longer it is touched.",
+    price: 2099,
+    discontinued: false,
     categories: ["Aqua"]
   }
 ];
